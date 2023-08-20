dc320951c555d601c4046574e260f80e55938946
PixelNass <nasserdevart@gmail.com>
2023-08-20 22:08:45 +0400

    add player movement

--- Sneeze Destruction/Assets/Scripts/TopDownPlayerController2D.cs
+++ Sneeze Destruction/Assets/Scripts/TopDownPlayerController2D.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownPlayerController2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 3f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    private PlayerControls playerControls;
+
+    private void Awake()
     {
-        
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
     }
 
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>().normalized;
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 }
